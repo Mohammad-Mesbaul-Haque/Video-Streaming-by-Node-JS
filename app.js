279dc4b453788eb2af50e061435c6ce71d55a585
mesba <mesbaul333@gmail.com>
2021-05-01 16:10:27 +0600

    app.js file added

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+ * Title: 
+ *  Description: 
+ *  Author: Mohammad Mesbaul Haque
+ * Date: 01/05/2021
+ *
+ */
+// Dependencies.
+
+
+
+// Module scaffolding.
+
+
+// main functions or objects.
+
+
+
+// export the module.
